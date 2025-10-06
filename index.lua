ab27deebbf8629ef97dec209c850f0f3a9c8f0e4
Oinite <tinioalfred@gmail.com>
2025-10-06 11:45:00 -0600

    Preparing for v0.1.0 release

--- index.lua
+++ index.lua
@@ -13,6 +13,10 @@ Potassium = Potassium or {}
 Potassium.mod_path = tostring(SMODS.current_mod.path)
 Glop_f = Glop_f or {}
 
+----------
+-- LOADING
+----------
+
 -- A shorthand of adding an event to G.E_MANAGER that only defines the properties trigger, delay, and func.\
 -- Event function will always return true, so "return true" is not required.\
 -- Consequently, do not use this function if the event function needs to return a non-true value\
@@ -51,6 +55,10 @@ Glop_f.load_directory("load_assets")
 Glop_f.load_directory("func")
 Glop_f.load_directory("items")
 
+-------------
+-- EVOLUTIONS
+-------------
+
 -- Add Ambrosia evolutions
 Potassium.banana_evolutions = Potassium.banana_evolutions or {}
 local k_map = Potassium.banana_evolutions
@@ -67,6 +75,10 @@ g_map["j_egg"]         = "j_kali_glegg"
 g_map["j_cavendish"]   = "j_kali_glopendish"
 g_map["j_diet_cola"]   = "j_kali_glop_cola"
 g_map["j_popcorn"]     = "j_kali_glop_corn"
+
+-------------------------------
+-- CALCULAITON KEY ORGANIZATION
+-------------------------------
 
 -- Manual list of calculation keys and their growth speed
 Potassium.calc_keys = Potassium.calc_keys or {}
@@ -118,4 +130,105 @@ for _, key in ipairs({
 }) do
 	Potassium.calc_keys.hyperoperative[key] = true
 	Potassium.calc_keys.all[key] = true
+end
+
+----------
+-- CREDITS
+----------
+
+local contrib_credits = {
+	{
+		'firz',
+		'zedruu_the_goat',
+		'vexastrae',
+		'playerrWon',
+		'Mysthaps',
+		'Foegro',
+		'pannella',
+		'Lexi',
+		'Project666',
+		'astrapboy',
+		'notmario',
+		'Mystic Misclick',
+		'Aomi',
+	}, {
+		'Crimson Heart',
+		'Squiddy',
+		'Dragokillfist',
+		'Jevonn',
+		'cassknows',
+		'ori',
+		'unexian',
+		'Aure',
+		'xphrogx',
+		'GloomyStew',
+		'George the Rat',
+		'5381',
+		'GiygasBandicoot',
+	}
+}
+
+local function prepare_credits(texts, align, text_scale)
+	local nodes = {}
+	for _, text in ipairs(texts) do
+		table.insert(nodes, {n=G.UIT.R, config={align = align, padding = 0}, nodes = {
+			{n=G.UIT.T, config={text = text, scale = text_scale*0.5, colour = G.C.UI.TEXT_LIGHT, shadow = true}}
+		}})
+	end
+	return {n=G.UIT.C, config={align = "tl", padding = 0.15, minw = 2.5}, nodes=nodes}
+end
+
+local function prepare_header(texts, text_scale)
+	local nodes = {}
+	for _, text in ipairs(texts) do
+		table.insert(nodes, {n=G.UIT.R, config={align = "cm", padding = 0}, nodes={
+			{n=G.UIT.T, config={text = text, scale = text_scale*0.8, colour = G.C.WHITE, shadow = true}},
+		}})
+	end
+	return {n=G.UIT.R, config={align = "cm", padding = 0.15, outline_colour = G.C.JOKER_GREY, r = 0.1, outline = 1}, nodes=nodes}
+end
+
+local function prepare_subheader(texts, text_scale)
+	local nodes = {}
+	for _, text in ipairs(texts) do
+		table.insert(nodes, {n=G.UIT.R, config={align = "cm", padding = 0}, nodes={
+			{n=G.UIT.T, config={text = text, scale = text_scale*0.6, colour = G.C.WHITE, shadow = true}},
+		}})
+	end
+	return {n=G.UIT.R, config={align = "cm", padding = 0.1, outline_colour = G.C.JOKER_GREY, r = 0.1, outline = 1}, nodes=nodes}
+end
+
+local function banana_credits()
+    local text_scale = 0.8
+    return {n=G.UIT.ROOT, config={align = "cm", padding = 0.2, colour = G.C.BLACK, r = 0.1, emboss = 0.05, minh = 6, minw = 6}, nodes={
+		{n=G.UIT.C, config={align = "cm", padding = 0.05}, nodes = {
+			prepare_header({"Remake Credits"}, text_scale),
+			prepare_subheader({
+				"Lead Developer: Oinite",
+				"Lead Designer: cassknows",
+			}, text_scale),
+		}},
+		{n=G.UIT.C, config={align = "cm", padding = 0.05}, nodes = {
+			prepare_header({"Original Potassium credits"}, text_scale),
+			prepare_subheader({"Lead Developer: MathIsFun_"}, text_scale),
+			{n=G.UIT.R, config={align = "cm", padding = 0.1,outline_colour = G.C.JOKER_GREY, r = 0.1, outline = 1}, nodes={
+				{n=G.UIT.R, config={align = "cm", padding = 0}, nodes={
+					{n=G.UIT.T, config={text = "Contributors", scale = text_scale*0.6, colour = G.C.BANAN1, shadow = true}},
+				}},
+				{n=G.UIT.R, config={align = "tm", padding = 0}, nodes={
+					prepare_credits(contrib_credits[1], 'cr', text_scale),
+					prepare_credits(contrib_credits[2], 'cl', text_scale),
+				}},
+			}}
+		}}
+	}}
+end
+
+SMODS.current_mod.extra_tabs = function ()
+	return {
+		{
+			label = "Credits",
+			tab_definition_function = banana_credits
+		}
+	}
 end
