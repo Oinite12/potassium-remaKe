33146bd6095feee9553f2b37b13729b2150c512a
Oinite <100444544+Oinite12@users.noreply.github.com>
2025-11-24 13:23:00 -0600

    MyDreamJournal modkeys compat

---
+++ cross-mod/MyDreamJournal.lua
@@ -0,0 +1,14 @@
+-- CROSS-MOD for My Dream Journal
+-- Content includes: N/A
+
+Potassium.glopmodkeys = {
+    ['glop'] = 'add',
+    ['xglop'] = 'mult',
+    ['eglop'] = 'expo',
+}
+
+Potassium.sfarkmodkeys = {
+    ['sfark'] = 'add',
+    ['xsfark'] = 'mult',
+    ['esfark'] = 'expo',
+}
